94b7abb8c23179c04a3fb9d5d9da954d965e803c
lars <lars.uffmann@vcsys.de>
2019-12-31 10:23:58 +0100

    refactor returnQName to PayloadRootUtils

--- spring-ws-core/src/main/java/org/springframework/ws/server/endpoint/adapter/method/jaxb/XmlRootElementPayloadMethodProcessor.java
+++ spring-ws-core/src/main/java/org/springframework/ws/server/endpoint/adapter/method/jaxb/XmlRootElementPayloadMethodProcessor.java
@@ -23,16 +23,16 @@ import javax.xml.bind.annotation.XmlType;
 import javax.xml.namespace.QName;
 
 import org.springframework.core.MethodParameter;
-import org.springframework.util.Assert;
 import org.springframework.ws.context.MessageContext;
 import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
+import org.springframework.ws.server.endpoint.support.PayloadRootUtils;
 
 /**
  * Implementation of {@link org.springframework.ws.server.endpoint.adapter.method.MethodArgumentResolver
  * MethodArgumentResolver} and {@link org.springframework.ws.server.endpoint.adapter.method.MethodReturnValueHandler
  * MethodReturnValueHandler} that supports parameters annotated with {@link XmlRootElement @XmlRootElement} or {@link
  * XmlType @XmlType}, and return values annotated with {@link XmlRootElement @XmlRootElement}.
- *
+ * <p>
  * Since version 3.0.9 return values annotated with {@link XmlType @XmlType} are supported. In order to marshall the
  * response message, the {@link ResponsePayload @ResponsePayload} annotation has been extended with the namespace and
  * localPart properties.
@@ -78,24 +78,9 @@ public class XmlRootElementPayloadMethodProcessor extends AbstractJaxb2PayloadMe
 			marshalToResponsePayload(messageContext, parameterType, returnValue);
 		}
 		else {
-			QName returnQname = returnQname(returnType);
+			QName returnQname = PayloadRootUtils.getResponsePayloadQName(returnType);
 			JAXBElement<?> element = new JAXBElement(returnQname, parameterType, returnValue);
 			marshalToResponsePayload(messageContext, parameterType, element);
 		}
 	}
-
-	protected QName returnQname(MethodParameter returnType) {
-		ResponsePayload responsePayload = returnType.getMethodAnnotation(ResponsePayload.class);
-		Assert.notNull(responsePayload, "could not resolve @ResponsePayload annotation from returnType");
-		Assert.hasText(responsePayload.localPart(), "localPart must not be empty");
-		final String localPart;
-		if (responsePayload.localPart().startsWith("+")) {
-			localPart = returnType.getMethod().getName() + responsePayload.localPart().substring(1);
-		}
-		else {
-			localPart = responsePayload.localPart();
-		}
-		return new QName(responsePayload.namespace(), localPart);
-	}
-
 }
--- spring-ws-core/src/test/java/org/springframework/ws/server/endpoint/support/PayloadRootUtilsTest.java
+++ spring-ws-core/src/test/java/org/springframework/ws/server/endpoint/support/PayloadRootUtilsTest.java
@@ -17,6 +17,8 @@
 package org.springframework.ws.server.endpoint.support;
 
 import java.io.StringReader;
+import java.lang.reflect.Method;
+
 import javax.xml.namespace.QName;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
@@ -34,12 +36,111 @@ import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.xml.sax.InputSource;
 
+import org.springframework.core.MethodParameter;
 import org.springframework.util.xml.StaxUtils;
+import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
+import org.springframework.ws.server.endpoint.annotation.RequestPayload;
+import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
+import org.springframework.xml.DocumentBuilderFactoryUtils;
 import org.springframework.xml.XMLInputFactoryUtils;
 import org.springframework.xml.transform.TransformerFactoryUtils;
-import org.springframework.xml.DocumentBuilderFactoryUtils;
 
 public class PayloadRootUtilsTest {
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testGetQNameFromRequestPayloadParameter_IAE() throws NoSuchMethodException {
+		Method underTest = PayloadRootUtilsTest.class.getMethod("testIAE", TestRequestType.class);
+		Assert.assertNotNull(underTest);
+		MethodParameter parameter = new MethodParameter(underTest, 0);
+		QName requestQname = PayloadRootUtils.getRequestPayloadQName(parameter);
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testGetQNameFromResponsePayloadParameter_IAE() throws NoSuchMethodException {
+		Method underTest = PayloadRootUtilsTest.class.getMethod("testIAE", TestRequestType.class);
+		Assert.assertNotNull(underTest);
+		MethodParameter returnType = new MethodParameter(underTest, -1);
+		QName responseQname = PayloadRootUtils.getResponsePayloadQName(returnType);
+		System.out.println(responseQname);
+	}
+
+	@Test
+	public void testGetQNameFromRequestPayloadParameter() throws NoSuchMethodException {
+		Method underTest = PayloadRootUtilsTest.class.getMethod("test", TestRequestType.class);
+		Assert.assertNotNull(underTest);
+		MethodParameter parameter = new MethodParameter(underTest, 0);
+		MethodParameter returnType = new MethodParameter(underTest, -1);
+		QName requestQname = PayloadRootUtils.getRequestPayloadQName(parameter);
+		QName responseQname = PayloadRootUtils.getResponsePayloadQName(returnType);
+		// localPart derived form method name
+		Assert.assertEquals( "testRequest", requestQname.getLocalPart());
+		// namespace uses default value from @PayloadRoot
+		Assert.assertEquals( "http://foo.bar/", requestQname.getNamespaceURI());
+
+		// localPart derived form method name
+		Assert.assertEquals("testResponse", responseQname.getLocalPart());
+		// namespace uses default value from @PayloadRoot
+		Assert.assertEquals( "http://foo.bar/", responseQname.getNamespaceURI());
+	}
+
+	@Test
+	public void testGetQNameFromRequestPayloadParameter_fun() throws NoSuchMethodException {
+		Method underTest = PayloadRootUtilsTest.class.getMethod("fun", TestRequestType.class);
+		Assert.assertNotNull(underTest);
+		MethodParameter parameter = new MethodParameter(underTest, 0);
+		MethodParameter returnType = new MethodParameter(underTest, -1);
+		QName requestQname = PayloadRootUtils.getRequestPayloadQName(parameter);
+		QName responseQname = PayloadRootUtils.getResponsePayloadQName(returnType);
+		Assert.assertEquals( "testRequest", requestQname.getLocalPart());
+		Assert.assertEquals( "http://foo.bar/", requestQname.getNamespaceURI());
+
+		Assert.assertEquals("testResponse", responseQname.getLocalPart());
+		Assert.assertEquals( "http://foo.bar/", responseQname.getNamespaceURI());
+	}
+
+	@Test
+	public void testGetQNameFromRequestPayloadParameter_fun2() throws NoSuchMethodException {
+		Method underTest = PayloadRootUtilsTest.class.getMethod("fun2", TestRequestType.class);
+		Assert.assertNotNull(underTest);
+		MethodParameter parameter = new MethodParameter(underTest, 0);
+		MethodParameter returnType = new MethodParameter(underTest, -1);
+		QName requestQname = PayloadRootUtils.getRequestPayloadQName(parameter);
+		QName responseQname = PayloadRootUtils.getResponsePayloadQName(returnType);
+		Assert.assertEquals( "funkyRequest", requestQname.getLocalPart());
+		Assert.assertEquals( "http://foo.bar/", requestQname.getNamespaceURI());
+
+		Assert.assertEquals("funkyResponse", responseQname.getLocalPart());
+		Assert.assertEquals( "http://foo.bar/", responseQname.getNamespaceURI());
+	}
+
+	public @ResponsePayload
+	TestResponseType testIAE(@RequestPayload TestRequestType myRequestType) {
+		return new TestResponseType();
+	}
+
+	@PayloadRoot(namespace = "http://foo.bar/", localPart = "testRequest")
+	public @ResponsePayload
+	TestResponseType test(@RequestPayload TestRequestType myRequestType) {
+		return new TestResponseType();
+	}
+
+	public @ResponsePayload(namespace = "http://foo.bar/", localPart = "testResponse")
+	TestResponseType fun(@RequestPayload(namespace = "http://foo.bar/", localPart = "testRequest") TestRequestType myRequestType) {
+		return new TestResponseType();
+	}
+
+	public @ResponsePayload(namespace = "http://foo.bar/", localPart = "funkyResponse")
+	TestResponseType fun2(@RequestPayload(namespace = "http://foo.bar/", localPart = "funkyRequest") TestRequestType myRequestType) {
+		return new TestResponseType();
+	}
+
+	class TestResponseType {
+
+	}
+
+	class TestRequestType {
+
+	}
 
 	@Test
 	public void testGetQNameForDomSource() throws Exception {
